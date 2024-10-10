4b64558cae3a55d19e9efce1361e991ed89204eb
camiielle <camillalazzati@gmail.com>
2024-10-10 16:49:42 +0200

    added last re-parametrization of model

--- climate_model.py
+++ climate_model.py
@@ -85,7 +85,7 @@ def callback(g):
         "Iteration: %i, tau: %f, |v| = %f, eigenvalue: %.8f"
         % (len(g.vs), g.ts[-1], np.linalg.norm(g.vs[-1]), s[-1])
     )
-    return np.linalg.norm(g.vs[-1]) < 80.0
+    return True #np.linalg.norm(g.vs[-1]) < 60.0
 
 
 # Construct the geodesic
@@ -152,6 +152,8 @@ def Avv_new(y, v_new):
 
 # Choose starting parameters
 y = [0.7/100, 3.9/8]
+print(r_new(y))
+print(j_new(y))
 v_new = initial_velocity(y, j_new, Avv_new)
 
 # Callback
@@ -173,3 +175,67 @@ geo_new = Geodesic(r_new, j_new, Avv_new, y, v_new, atol=1e-2, rtol=1e-2,
 geo_new.integrate(480)
 
 plot_geodesic_path(geo_new, colors[2:], labels[2:], N_new)
+
+# define new model (M=6,N=1)
+N_new_new = 1
+def r_new_new(F):
+    λ = 1e10
+    γ = 1e10
+    γ0 = 1e10
+
+    # General parameters
+    b = λ + γ + γ0
+    b_star = λ + γ - γ0
+    delta = b*b - 4 * λ * γ0
+
+    # Mode parameters (Fast and Slow)
+    τ_f = (b - np.sqrt(delta)) / (2 * γ0 * λ)
+    τ_s = (b + np.sqrt(delta)) / (2 * γ0 * λ)
+
+    φ_s = 1 / (2 * γ) * (b_star + np.sqrt(delta))
+    φ_f = 1 / (2 * γ) * (b_star - np.sqrt(delta))
+
+    a_f = φ_s * τ_f * λ / (φ_s - φ_f)
+    l_f = a_f * τ_f * λ / (1 + γ/γ0)
+
+    a_s = -φ_f * τ_s * λ / (φ_s - φ_f)
+    l_s = a_s * τ_s * λ / (1 + γ/γ0)
+
+    # defining ODEs solutions
+    Ta = F/λ*(a_f*(1-np.exp(-1/τ_f*t)) + a_s*(1-np.exp(-1/τ_s*t)))
+    To = F/λ*(a_f*φ_f*(1-np.exp(-1/τ_f*t)) + φ_s*a_s*(1-np.exp(-1/τ_s*t)))
+
+    return np.hstack((Ta, To))
+
+def j_new_new(F):
+    jacob = jacobian_func(r_new_new, M, N_new_new)
+    return jacob(F)
+
+# Directional second derivative
+def Avv_new_new(F, v_new_new):
+    avv_function = Avv_func(r_new_new)
+    return avv_function(F, v_new_new)
+
+# Choose starting parameters
+F= np.array([3.9/8])
+v_new_new = initial_velocity(F, j_new_new, Avv_new_new)
+
+# Callback
+def callback_new_new(g):
+    # Integrate until the norm of the velocity has grown by a factor of 10
+    # and print out some diagnotistic along the way
+    _, s, _ = np.linalg.svd(j_new_new(g.xs[-1]))
+    print(
+        "Iteration: %i, tau: %f, |v| = %f, eigenvalue: %.25f"
+        % (len(g.vs), g.ts[-1], np.linalg.norm(g.vs[-1]), s[-1])
+    )
+    return True
+
+# Construct the geodesic
+geo_new_new = Geodesic(r_new_new, j_new_new, Avv_new_new, F, v_new_new, atol=1e-2, rtol=1e-2,
+               parameterspacenorm=False, callback=callback_new_new)
+
+# Integrate
+geo_new_new.integrate(480)
+
+plot_geodesic_path(geo_new, colors[3:], labels[3:], N_new_new)
